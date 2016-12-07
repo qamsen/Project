a60f16a81f21f5ef2adfd5b627a0f3d9ddb99606
Qamsen <krd42@case.edu>
2016-12-06 21:09:36 -0500

    Updated comments and added 'isValidInput' method.

--- VisualSorting.java
+++ VisualSorting.java
@@ -2,23 +2,25 @@ import Algorithms.*;
 
 public class VisualSorting {
 
-    
-    List validSorts = Arrays.toList(
-            {"mergesort", "heapsort", "insertionsort", "quicksort", "shellsort"});
-
     public static void main(String[] args) {
 
+        /* Checks to make sure the inputs are valid */
         validInputs = false;
+
+        /* The name of the sorting algorithm the user input */
         String sortName;
+
+        /* Array length the user input */
         int arrayLength;
 
+        /* Checks the sorting algorithm name */
         try {
-            sortName = parseString(args[0]);
-            validInputs = validSorts.contains(sortName);
+            validInputs = isValidInput(args[0]);
         }
         catch (NullPointerException e) {
         }
 
+        /* Checks that the array length is valid */
         try {
             arrayLength = String.parseInt(args[1]);
             validInputs = true;
@@ -26,14 +28,27 @@ public class VisualSorting {
         catch (NullPointerException e || NumberFormatException f) {
         }
 
+        /* Runs program when an algorithm and array length is supplied */
         if (validInputs) {
             launchProgram(sortName, arrayLength);
         }
-        else System.out.println("Please give valid inputs"); }
 
+        /* Error catching */
+        else
+            System.out.println("Please give valid inputs");
+    }
+
+    /**
+     * Runs the sorting algorithm
+     *
+     * @param sortName    the name of the sorting algorithm being run.
+     * @param arrayLength the length of the array the program will sort.  */
     public static void launchProgram(String sortName, int arrayLength) {
+
+        /* Creates a random array to be sorted */
         Integer[] arr = randomArray(arrayLength);
 
+        /* Runs the given sorting algorithm */
         switch (sortName) {
             case 1: sortName = "mergesort";
                     MergeSort sort = new MergeSort(arr);
@@ -53,12 +68,27 @@ public class VisualSorting {
         }
     }
 
+    /**
+     * Removes all whitespaces in the String and decapitalizes it.
+     *
+     * @param s the String being stripped.
+     * @return  the String with no capital letters or whitespace.
+     */
     public static String parseString(String s) {
+
         s = s.toLowerCase();
         s = s.replaceAll("\\s+","");
+
+        return s;
     }
 
-    public static Integer[] randomArray(int arrayLength) {
+    /**
+     * Returns a randomly sorted array.
+     *
+     * @param arrayLength the length of the random array.
+     * @return            a randomly sorted array.
+     */
+     * public static Integer[] randomArray(int arrayLength) {
 
         Integer[] arr = new Integer[arrayLength];
 
@@ -66,7 +96,27 @@ public class VisualSorting {
             arr[i] = i;
         }
 
+        /* Randomizes the contents of the array */
         return Collections.shuffle(arr);
+    }
+
+    /**
+     * Checks to see if a String is the name of a sorting algorithm in this
+     * program.
+     *
+     * @param sortName the String that is being checked.
+     * @return         if the String is the name of a sorting algorithm.
+     */
+    private static boolean isValidSort(String sortName) {
+
+        /* The valid sorting algorithms in this program */
+        List validSorts = Arrays.toList(
+                {"mergesort", "heapsort", "insertionsort", "quicksort", "shellsort"});
+
+        /* Removes white space and decapitalizes the String */
+        sortName = parseString(sortName);
+
+        return validSorts.contains(sortName);
     }
 
 }
